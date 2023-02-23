2d1bd261eab1c1673d6f91c8b5d2e0531c633d4b
d_dubois <dubois.dorian2@gmail.com>
2023-02-23 13:43:31 +0100

    Dorian - Ajout acces configbdd.properties pour recuperer données BDD

--- src/main/java/bdd/DatabaseSingleton.java
+++ src/main/java/bdd/DatabaseSingleton.java
@@ -3,29 +3,54 @@ package bdd;
 
 import constantes.Constants;
 
+import java.io.*;
+import java.nio.file.Path;
 import java.sql.*;
+import java.util.Properties;
 
 
 public class DatabaseSingleton {
 
+
+    //RECUPERATION DES DONNEES DE LA BDD
+
+
     private static final DatabaseSingleton instance;
 
     static {
-        instance = new DatabaseSingleton();
+        try {
+            instance = new DatabaseSingleton();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private Connection connection;
 
-    private DatabaseSingleton() {
+    private DatabaseSingleton() throws FileNotFoundException {
         System.out.println("Instanciation du Singleton");
+        System.out.println("Path executing => "+System.getProperty("user.dir"));
     }
 
     public static DatabaseSingleton getInstance() {
         return instance;
     }
 
-    public void connect() throws SQLException {
-        connection = DriverManager.getConnection(Constants.url, Constants.user, Constants.psw);
+    public void connect() throws SQLException, IOException {
+        try (InputStream input = new FileInputStream(System.getProperty("user.dir")+"/configbdd.properties")){
+            Properties prop = new Properties();
+            prop.load(input);
+            String url = prop.getProperty("db.url");
+            String user = prop.getProperty("db.user");
+            String password = prop.getProperty("db.password");
+            System.out.println("url : " + url +"user : " + user + "psw : " + password );
+            connection = DriverManager.getConnection(url,user, password);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
     }
 
     public ResultSet query(String sql) throws SQLException {
